a9a8822623de7bf3526c47b331e87237b4a60ec6
Shubham <64083206+sk244194@users.noreply.github.com>
2026-03-19 01:01:57 +0530

    Added_Explicit_wait

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
@@ -12,8 +14,16 @@ driver.find_element(By.ID,"password").send_keys("Password123")
 driver.find_element(By.ID,"submit").click()
 
 
-time.sleep(8)
+# time.sleep(8)
+# This time method is not preferred use implicit or explicit waits
 
-assert "logged-in-successfully" in driver.current_url
+wait = WebDriverWait(driver,10)
+
+logout_button = wait.until(EC.visibility_of_element_located((By.LINK_TEXT,"Log out")))
+
+# assert "logged-in-successfully" in driver.current_url
+# This will print error if something wrong otherwise nothing
+
+assert logout_button.is_displayed()
 
 driver.quit()
