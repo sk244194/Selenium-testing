b213cb83d7582689373315834afcb1c957a4aa42
Shubham <64083206+sk244194@users.noreply.github.com>
2026-03-23 12:12:23 +0530

    added pytest

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,31 +7,34 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pages.login_page import LoginPage
 
-driver = webdriver.Chrome()
-driver.get("https://practicetestautomation.com/practice-test-login/")
 
-wait = WebDriverWait(driver, 10)
+def test_valid_login():
+    # Moved everything inside this function to check pytest functionaliy(just write pytest to call all the test functions automatically)
+    driver = webdriver.Chrome()
+    driver.get("https://practicetestautomation.com/practice-test-login/")
 
-login = LoginPage(driver)
+    wait = WebDriverWait(driver, 10)
+
+    login = LoginPage(driver)
 
 
-try:
-    login.enterUsername("student")
-    login.enterPassword("Password123")
-    login.clickLogin()
+    try:
+        login.enterUsername("student")
+        login.enterPassword("Password123")
+        login.clickLogin()
 
-    wait.until(EC.visibility_of_element_located(login.logoutBtn))
+        wait.until(EC.visibility_of_element_located(login.logoutBtn))
 
-    if (
-        "logged-in-successfully" in driver.current_url
-        and login.isLogoutVisible()
-        and "Logged In Successfully" in driver.page_source
-    ):
-        print("Login Successful")
-    else:
+        if (
+            "logged-in-successfully" in driver.current_url
+            and login.isLogoutVisible()
+            and "Logged In Successfully" in driver.page_source
+        ):
+            print("Login Successful")
+        else:
+            print("Login Failed")
+
+    except:
         print("Login Failed")
 
-except:
-    print("Login Failed")
-
-driver.quit()
+    driver.quit()
